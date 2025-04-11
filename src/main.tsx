99a7f2583e6b629d70d73ab92081cddedfcfff73
HearDiary <laukomiro2@gmail.com>
2025-04-11 06:54:46 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>HearDiary MVP</h1>
+    <p>This is a minimal app.</p>
+  </React.StrictMode>,
+)
