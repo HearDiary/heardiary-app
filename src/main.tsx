0f8b4b0c86b38506dbb45b88a5cbe5f9b3312cdb
HearDiary <laukomiro2@gmail.com>
2025-04-14 17:36:29 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -111,7 +111,7 @@ const App = () => {
 
   const stopRecording = () => {
     if (mediaRecorderRef.current?.state !== 'inactive') {
-      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current?.stop();
     }
   };
 
